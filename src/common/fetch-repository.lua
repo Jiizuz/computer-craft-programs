270dbc5aa5185cc0051447e8e410358c23a35247
Jiizuz <masterchack92@hotmail.com>
2025-03-08 15:58:46 -0600

    fix(fetch-utility): distinguish between files and path in order to flatten the files in root dir

--- src/common/fetch-repository.lua
+++ src/common/fetch-repository.lua
@@ -23,19 +23,24 @@ end
 -- Repository resolve functions
 
 --- Returns the formatted URL of the file in the repository
-local function format_file_url(file_name, repository)
+local function format_file_url(path, repository)
     if repository == FetchRepository.Repository.GITHUB then
-        return github_url_file(file_name)
+        return github_url_file(path)
     end
     error("Unknown repository: " .. repository)
 end
 
+--- Returns the file name without the path
+local function strip_file_name(path)
+    return path:match("([^/]+)$")
+end
+
 --- Fetches the files from the repository
 function FetchRepository.fetch_files(files, repository)
-    delete_and_fetch_file("common/fetch-repository.lua",
+    delete_and_fetch_file("fetch-repository.lua",
             format_file_url("common/fetch-repository.lua", repository))
-    for _, file in ipairs(files) do
-        delete_and_fetch_file(file, format_file_url(file, repository))
+    for _, path in ipairs(files) do
+        delete_and_fetch_file(strip_file_name(path), format_file_url(path, repository))
     end
 end
 
