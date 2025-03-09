254b9307f8325a096360565ae4ddf975484001e2
Jiizuz <masterchack92@hotmail.com>
2025-03-08 19:08:55 -0600

    feat(place-dig-timed): place and dig timed program main logic added

---
+++ src/place-dig-timed/place-dig-timed.lua
@@ -0,0 +1,20 @@
+-- Places and digs an item continuously with a timed delay
+
+-- Constants
+
+--- The delay between placing and digging an item
+local DIG_DELAY = 1
+--- The delay between digging and placing an item
+local PLACE_DELAY = 90
+
+-- Main functions
+
+--- Places and digs an item with a timed delay
+function place_and_dig()
+    turtle.select(1) -- Select the first slot to place and dig
+
+    turtle.place()
+    os.sleep(DIG_DELAY)
+    turtle.dig()
+    os.sleep(PLACE_DELAY)
+end
